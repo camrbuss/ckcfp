16a3178eba34aa6554ac08063f94e2b2d8149680
camrbuss <camrbuss@vt.edu>
2021-08-07 18:12:55 -0600

    3d model download script

---
+++ download_3d_models.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -o ./modules.pretty/pico-r3.zip https://datasheets.raspberrypi.org/pico/Pico-R3-step.zip
+unzip ./modules.pretty/pico-r3.zip -d ./modules.pretty
+
+curl -o ./connectors.pretty/54-00177.STEP https://tensility.s3-us-west-2.amazonaws.com/uploads/3dmodels/54-00177.STEP
+
