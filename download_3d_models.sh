daa9a37c3fc60c49d96bf140c78a0c67b7f7241b
camrbuss <camrbuss@vt.edu>
2021-08-15 20:46:27 -0600

    mounting holes

--- download_3d_models.sh
+++ download_3d_models.sh
@@ -5,3 +5,5 @@ unzip ./modules.pretty/pico-r3.zip -d ./modules.pretty
 
 curl -o ./connectors.pretty/54-00177.STEP https://tensility.s3-us-west-2.amazonaws.com/uploads/3dmodels/54-00177.STEP
 
+curl -o ./mounting.pretty/90611A112.STEP https://www.mcmaster.com/mvC/library/20200221/90611A112_SCREW-MOUNT%20NUT.STEP
+
