17c3a884a4eb1961cdafadd242ac63b54b447dad
camrbuss <camrbuss@vt.edu>
2023-02-19 17:49:48 -0800

    3D model path corrections

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ckcfp
 Custom KiCad Footprints
+
+## KiCad Environment Variables
+Set `CKCFP_HOME` to the location of this repository
--- download_3d_models.sh
+++ download_3d_models.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-curl -o ./modules.pretty/pico-r3.zip https://datasheets.raspberrypi.org/pico/Pico-R3-step.zip
-unzip ./modules.pretty/pico-r3.zip -d ./modules.pretty
+curl -o ./ckcfp-modules.pretty/Pico-R3-step.zip https://datasheets.raspberrypi.org/pico/Pico-R3-step.zip
+unzip ./ckcfp-modules.pretty/Pico-R3-step.zip -d ./ckcfp-modules.pretty
 
-curl -o ./connectors.pretty/54-00177.STEP https://tensility.s3-us-west-2.amazonaws.com/uploads/3dmodels/54-00177.STEP
+curl -o ./ckcfp-connectors.pretty/54-00177.STEP https://tensility.s3-us-west-2.amazonaws.com/uploads/3dmodels/54-00177.STEP
 
-curl -o ./mounting.pretty/90611A112.STEP https://www.mcmaster.com/mvC/library/20200221/90611A112_SCREW-MOUNT%20NUT.STEP
+curl -o ./ckcfp-mounting.pretty/90611A112.STEP https://www.mcmaster.com/mvC/library/20200221/90611A112_SCREW-MOUNT%20NUT.STEP
+
+curl -o ./ckcfp-switches.pretty/pec11r-4x20f-sxxxx.stp https://www.bourns.com/engineering/PEC11R/pec11r-4x20f-sxxxx.stp
 
